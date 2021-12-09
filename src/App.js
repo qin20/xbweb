8d7269c235f7421f68255487be4dd53792fc364d
qin20 <qinyb@foxmail.com>
2021-12-10 03:23:14 +0800

    save

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://xb.vengogh.com/小白剪辑 Setup 1.0.0.exe"
           target="_blank"
           rel="noopener noreferrer"
         >
